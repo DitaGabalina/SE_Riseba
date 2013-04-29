09e5afc6f4a531d2837fe9b9b1e42a6574376004
Dita Gabalina <dita.gabalina@gmail.com>
2013-04-29 23:14:31 +0300

    Log4j Example

---
+++ CASE_examples/src/Log4JSample.java
@@ -0,0 +1,25 @@
+import org.apache.log4j.Logger;
+
+public class Log4JSample {
+	
+	private static Logger logger=Logger.getLogger("DemoLogger");
+
+	public static void main(String[] args) {
+
+		logger.debug("Log4JSample: method Start");
+//		System.out.println("method start");
+		
+		String[] strArray = new String[3];
+
+		try {
+			strArray[4] = "No element here!";
+		} catch (ArrayIndexOutOfBoundsException e) {
+//			e.printStackTrace();
+			logger.error("Error Occured due to " ,e);
+		}
+
+		logger.debug("Log4JSample: method end");
+//		System.out.println("method End");
+
+	}
+}
